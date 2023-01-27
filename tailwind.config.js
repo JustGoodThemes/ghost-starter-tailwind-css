412bd50fd2f13ec4ea21f72e64b8532ee2ae89f3
astab <astabankauske@gmail.com>
2023-01-27 14:54:39 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.hbs", "./**/*.hbs"],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/typography')
+  ],
+}
